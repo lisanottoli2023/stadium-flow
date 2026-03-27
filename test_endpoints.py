8d69e6488c48ddff6aa3eb921f0c88a76ca3c692
lisanottoli2023 <lisa.nottoli@epitech.eu>
2026-03-27 16:47:50 +0100

    Add player data and standings API samples, update dependencies, and create test for standings endpoint
    - Added `players.json` with player data for the 2024 Premier League season.
    - Added `standings.json` with a placeholder for standings data.
    - Updated `pyproject.toml` to include `python-dotenv` as a dependency.
    - Created `test_endpoints.py` to test the standings API endpoint and save the response to a JSON file.
    - Updated `uv.lock` to include the new `python-dotenv` dependency.

---
+++ test_endpoints.py
@@ -0,0 +1,23 @@
+import requests
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+
+url = "https://v3.football.api-sports.io/fixtures/standings"
+headers = {
+    "x-apisports-key": os.getenv("API_KEY")
+}
+
+params = {
+    "league": 39,
+    "season": 2024
+}
+
+response = requests.get(url, headers=headers, params=params)
+print(response.json())
+#create json file 
+with open("standings.json", "w") as f:
+    f.write(response.text)
+
